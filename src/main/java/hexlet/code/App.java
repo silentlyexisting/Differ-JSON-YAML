9f9c3b7d25427189ebffe51f9775598ba0658e6a
silentlyexisting <silentlyexistin@gmail.com>
2021-10-21 00:42:04 +0300

    work with json step 1

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,14 +6,13 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "version 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable<String> {
   @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
   private String format = "stylish";
 
@@ -23,8 +22,14 @@ public class App {
   @Parameters(index = "0", description = "path to second file")
   private File filepath2;
 
+  @Override
+  public String call() throws Exception { // your business logic goes here...
+    return Differ.generate(filepath1, filepath2);
+  }
+
   public static void main(String[] args) {
     int exitCode = new CommandLine(new App()).execute(args);
     System.exit(exitCode);
+
   }
 }
--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -2,17 +2,14 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import picocli.CommandLine;
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
-public class Differ {
-    public static String generate() throws Exception {
-
+public class Parser {
+    public static String parser() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
 
         return new String();
     }
