9da04914fb0916d833eaa4703daf5aa6f936db5a
silentlyexisting <silentlyexistin@gmail.com>
2021-10-20 19:46:38 +0300

    picocli setting step 1

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -14,7 +14,14 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "version 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+  @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+  private String format = "stylish";
 
+  @Parameters(index = "0", description = "path to first file")
+  private File filepath1;
+
+  @Parameters(index = "0", description = "path to second file")
+  private File filepath2;
 
   public static void main(String[] args) {
     int exitCode = new CommandLine(new App()).execute(args);
