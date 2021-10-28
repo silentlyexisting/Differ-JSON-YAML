894fe49d23e31535aa7165533a551f99481b4f5b
silentlyexisting <silentlyexistin@gmail.com>
2021-10-28 22:59:56 +0300

    little fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,12 +6,13 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "version 1.0",
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        version = "version 1.0",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<String> {
-  @Parameters(index = "0", description = "path to first file")
-  private String filepath1;
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
 
   @Parameters(index = "1", description = "path to second file")
   private String filepath2;
