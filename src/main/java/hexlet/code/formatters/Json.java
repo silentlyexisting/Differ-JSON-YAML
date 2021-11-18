f3f7b62a30a93de7e07d1c42a70c39a863b762e8
silentlyexisting <silentlyexistin@gmail.com>
2021-11-18 19:52:29 +0200

    finalization of the diff

--- src/main/java/hexlet/code/formatters/Json.java
+++ src/main/java/hexlet/code/formatters/Json.java
@@ -3,8 +3,8 @@ package hexlet.code.formatters;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Json {
     public static String buildJsonFormat(List<Map<String, Object>> diff) throws JsonProcessingException {
--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 class AppTest {
     private final Path flatJsonPath = Path.of(getFixturesPath("stylishExpected"));
