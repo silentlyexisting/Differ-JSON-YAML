9122ea483552c44d08f406b252b1adac1bfb706c
silentlyexisting <silentlyexistin@gmail.com>
2021-11-18 21:50:47 +0200

    changed json formatter

--- src/main/java/hexlet/code/formatters/Json.java
+++ src/main/java/hexlet/code/formatters/Json.java
@@ -8,14 +8,6 @@ import java.util.Map;
 
 public class Json {
     public static String buildJsonFormat(List<Map<String, Object>> diff) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("{\n" + "  \"diffs\": [\n");
-        for (Map<String, Object> map : diff) {
-            stringBuilder.append("    " + objectMapper.writeValueAsString(map) + ",\n");
-        }
-        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
-        stringBuilder.append("  ]\n" + "}");
-        return stringBuilder.toString();
+        return new ObjectMapper().writeValueAsString(diff);
     }
 }
